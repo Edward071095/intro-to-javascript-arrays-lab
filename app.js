ab54517d613e7ddf1b7eab469ed41c1c4bea5396
EdwardW710 <EdwardW710@yahoo.com>
2025-01-26 19:11:52 -0500

    arrays exercise 6

--- app.js
+++ app.js
@@ -65,8 +65,22 @@ Exercise 5: Insert an element between two others
 Complete Exercise 5 in the space below:
 */
 
-const foods = ['taco', 'pizza', 'cheeseburger'];
+//const foods = ['taco', 'pizza', 'cheeseburger'];
 
-foods.splice(2, 0, 'tofu');
+//foods.splice(2, 0, 'tofu');
 
-console.log('Exercise 5 result:', foods);
+//console.log('Exercise 5 result:', foods);
+
+/*
+Exercise 6: Replace elements
+
+1) Replace 'pizza' in the `foods` array with 'sushi' and 'cupcake'.
+
+Complete Exercise 6 in the space below:
+*/
+
+const foods = ['taco', 'pizza', 'tofu', 'cheeseburger'];
+
+foods.splice(1, 1, 'sushi', 'cupcake');
+
+console.log('Exercise 6 result:', foods);
